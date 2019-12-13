bfb2a8b4a44d5fc0c629236576ec9be902b52be2
Losijacopo <jacopo.losi@studenti.unitn.it>
2019-12-13 17:20:06 +0200

    Update Norm_calculation.m

--- MATLAB Code and Simulink_corrected/Norm_calculation.m
+++ MATLAB Code and Simulink_corrected/Norm_calculation.m
@@ -4,7 +4,7 @@ clear all;
 
 %% Loading of the files
 FileName   = '50_H.mat';
-FolderName = 'C:\Temp';
+FolderName = 'C:\Users\jlosi\OneDrive\Documenti\GitHub\DigitalOptimal-Project\MATLAB Code and Simulink_corrected\experiments_every_20';
 File       = fullfile(FolderName, FileName);
 load(File);   % not: load('File')
 
